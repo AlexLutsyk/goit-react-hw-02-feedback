b2c52bd96dd6bccde294df2d548b2aa7ec3dcb2b
Alex <ajlexmaritime@gmail.com>
2021-08-22 11:50:25 +0300

    end first task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import PropTypes from "prop-types";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
+import Notification from './components/Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  static propTypes = {
+    good: PropTypes.number,
+    neutral: PropTypes.number,
+    bad: PropTypes.number,
+  }
+
+   countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+   };
+  
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good  / this.countTotalFeedback()) * 100);
+  };
+
+
+  onLeaveFeedback = (evt) => {
+    
+    this.setState(prevState => ({
+      [evt]: prevState[evt] + 1,
+    }));
+  };
+
+
+  render() {
+    return (
+      <>
+        <Section title= "Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={ this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message={'No feedback given'} />
+          ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </>
+    )
+  }
+
+  
+
 }
 
 export default App;
